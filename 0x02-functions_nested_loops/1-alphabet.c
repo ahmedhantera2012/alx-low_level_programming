69d58ee51ef2333547b43baf525c27b83033d5bc
Ahmed Hantera <ahmedhantera1911@gmail.com>
2023-09-13 17:34:41 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: print in alphabet
+ * Return: void
+*/
+
+void print_alphapet(void);
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
